2977d939c38648b64f84899814c04101b86d46bf
joseDeuteros <josewbmx@gmail.com>
2018-03-06 21:16:19 -0600

    Se agregó la librería de facebook

--- wp-content/themes/discussionwp/header.php
+++ wp-content/themes/discussionwp/header.php
@@ -9,7 +9,16 @@
     ?>
 	<?php do_action('discussion_header_meta'); ?>
 
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
+    
+    <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.7&appId=490227714445389";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 </head>
 <body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
 <?php discussion_get_side_area(); ?>
